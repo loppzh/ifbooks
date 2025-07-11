bda592c68f41fcbb5decfb34e8b73dbd02759042
Hugo Manoél de Souza Lopes <162646686+loppzh@users.noreply.github.com>
2025-07-11 10:43:22 -0300

    cart.js

--- src/stores/cart.js
+++ src/stores/cart.js
@@ -3,12 +3,41 @@ import { ref } from 'vue'
 
 export const useCartStore = defineStore('cart', () => {
   const cart = ref({
-      items: [],
-      total: 0,
-       })
-       function addBook(book) {
-        console.log(book)
-       }
-    return { cart, addBook }
-    })
+    items: [],
+    total: 0,
+  })
 
+  function addBook(book) {
+    console.log(book)
+  }
+
+  function addToCart(book) {
+    const existingBook = cart.value.items.find((item) => item.id === book.id);
+    if (existingBook) {
+      existingBook.quantity++;
+    } else {
+      cart.value.items.push({ ...book, quantity: 1 });
+    }
+    cart.value.total += book.price;
+    alert(`Adicionado ${book.title} ao carrinho!`);
+  }
+
+  function decrementBookToCart(book) {
+    const existingBook = cart.value.items.find((item) => item.id === book.id);
+    if (existingBook.quantity === 1) {
+      cart.value.items = cart.value.items.filter((item) => item.id !== book.id);
+    } else {
+      existingBook.quantity--;
+    }
+    cart.value.total -= book.price;
+  }
+
+  function incrementBookToCart(book) {
+    const existingBook = cart.value.items.find((item) => item.id === book.id);
+    existingBook.quantity++;
+    cart.value.total += book.price;
+  }
+
+
+  return { cart, addBook, addToCart, decrementBookToCart, incrementBookToCart }
+})
